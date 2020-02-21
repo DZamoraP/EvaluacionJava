a04af36437b03cab3ef44803ae39e65aed37a61a
DZamoraP <57510822+DZamoraP@users.noreply.github.com>
2020-02-21 12:33:31 -0300

    Create Electrodomestico.java
    Primer Commit, creación superClase

---
+++ Electrodomestico.java
@@ -0,0 +1,46 @@
+package Electrodomestico;
+
+public class Electrodomestico {
+	
+	private  final static int PRECIOBASE = 100000;
+	private  final static String COLOR = "blanco";
+	private  final static String CONSUMOENERGETICO = "F";
+	private  final static int PESO = 5;
+	
+		int precioBase;
+		String color;
+		String consumoEnergetico;
+		int peso;
+		
+	
+	public Electrodomestico(int precioBase, String color, String consumoEnergetico, int peso) {
+	
+			this.precioBase = PRECIOBASE;
+			this.color = COLOR;
+			this.consumoEnergetico = CONSUMOENERGETICO;
+			this.peso = PESO;
+		}
+	
+	public Electrodomestico(String color ,String consumoEnergetico) {
+		
+		this.precioBase = PRECIOBASE;
+		this.color = color;
+		this.consumoEnergetico = consumoEnergetico;
+		this.peso = PESO;
+	}
+	
+	public Electrodomestico () {
+		
+	}
+	
+	public String comprobarConsumoEnergetico(String letra){
+		return letra;
+	}
+	
+	public String comprobarColor(String color) {
+		return color;
+	}
+
+		
+	
+}
