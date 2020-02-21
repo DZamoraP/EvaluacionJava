90a143b82dc5a4bfaf1156b4ab0e55b5e5f7e9d8
Alumno <Alumno@PC-PC>
2020-02-21 13:41:12 -0300

    crea clase Lavadora

---
+++ Lavadora.java
@@ -0,0 +1,29 @@
+
+public class extends Lavadora {
+	
+	private final static int CARGA= 5;
+
+}
+
+/**
+ * Atributos por defecto.
+ */
+
+	int carga;
+	
+	
+	public Electrodomestico(int carga) {
+		
+		this.carga = CARGA;
+	
+	
+	public Electrodomestico(int carga) {
+		this carga = carga;
+	}
+	
+		
+	public Electrodomestico() {
+		
+	}
+
+
