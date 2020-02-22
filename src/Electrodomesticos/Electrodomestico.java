17cd4bbc4b9028bb3f5c0f189705f98b2a1210b2
Daniel Zamora <danielz.informatico@gmail.com>
2020-02-22 12:01:05 -0300

    actualizados electrodimestico y lavadora

--- src/Electrodomesticos/Electrodomestico.java
+++ src/Electrodomesticos/Electrodomestico.java
@@ -16,14 +16,14 @@ package Electrodomesticos;
 public class Electrodomestico {
 	
 	
-	private  final static int PRECIOBASE = 100000;
+	private  final static int PRECIOBASE = 100;
 	private  final static String COLOR = "blanco";
 	private  final static String CONSUMOENERGETICO = "F";
 	private  final static int PESO = 5;
 	
-	
+		
 	/**
-	 * Atributos por defecto.
+	 * Atributos
 	 */
 	
 		int precioBase;
@@ -31,24 +31,15 @@ public class Electrodomestico {
 		String consumoEnergetico;
 		int peso;
 		
-	/**
-	 * Constructor por defecto.
-	 */
-		
-	public Electrodomestico () {
-
-	}
-	
-	
 	 /**
-     * Constructor con 4 parametros
+     * Constructor por defect con 4 parametros
      * @param precioBase  precio del electrodomestico
      * @param color  define color del electrodomestico
      * @param consumoEnergetico define el consumo entre A y F
      * @param peso define el peso del electrodomestico
      */
 	
-	public Electrodomestico(int precioBase, String color, String consumoEnergetico, int peso) {
+	public  Electrodomestico() {
 	
 		this.precioBase = PRECIOBASE;
 		this.color = COLOR;
@@ -59,32 +50,43 @@ public class Electrodomestico {
 	/**
  	* Constructor con parametros, 2 se declaras en base a constante y 2 por defecto
  	* @param precioBase  se define en base a valor constante
- 	* @param color  define color del electrodomestico
- 	* @param consumoEnergetico define el consumo entre A y F
+ 	* @param color  define color del electrodomestico por defecto como blanco
+ 	* @param consumoEnergetico define el consumo entre A y F por defecto como F
  	* @param peso define el peso del electrodomestico en base a atributo constante
  	*/
 	
-	public Electrodomestico(String color ,String consumoEnergetico) {
+	public Electrodomestico(int precioBase, int peso) {
 		
-		this.precioBase = PRECIOBASE;
-		this.color = color;
-		this.consumoEnergetico = consumoEnergetico;
-		this.peso = PESO;
+		this.precioBase = precioBase;
+		this.color = COLOR;
+		this.consumoEnergetico = CONSUMOENERGETICO;
+		this.peso = peso;
 	}
 	
 	/**
-	 * Constructor por defecto.
+	 * Constructor con todos los atributos.
 	 */
 		
-
+	public Electrodomestico(int precioBase, String color, String consumoEnergetico, int peso) {
+		
+		this.precioBase = precioBase;
+		this.color = color;
+		this.consumoEnergetico = consumoEnergetico;
+		this.peso = peso;
+	}
+	
+	
+	/**
+	 * Se defienen todos los Getters.
+	 */
+	
+	
+	
 	public int getPrecioBase() {
 		return precioBase;
 	}
 
 
-	public void setPrecioBase(int precioBase) {
-		this.precioBase = precioBase;
-	}
 
 
 	public String getColor() {
@@ -92,45 +94,17 @@ public class Electrodomestico {
 	}
 
 
-	public void setColor(String color) {
-		this.color = color;
-	}
-
 
 	public String getConsumoEnergetico() {
 		return consumoEnergetico;
 	}
 
 
-	public void setConsumoEnergetico(String consumoEnergetico) {
-		this.consumoEnergetico = consumoEnergetico;
-	}
 
 
 	public int getPeso() {
 		return peso;
 	}
-
-
-	public void setPeso(int peso) {
-		this.peso = peso;
-	}
-
-
-	public static int getPreciobase() {
-		return PRECIOBASE;
-	}
-
-
-
-
-
-	public static String getConsumoenergetico() {
-		return CONSUMOENERGETICO;
-	}
-
-
-
 
 
 	/**
@@ -146,9 +120,9 @@ public class Electrodomestico {
 	}
 	
 	
-//	public precioFinal() {
-//		
-//	}
+	public int precioFinal(int precio) {
+		return precio;
+	}
 
 		
 	
--- src/Electrodomesticos/Lavadora.java
+++ src/Electrodomesticos/Lavadora.java
@@ -3,49 +3,68 @@ package Electrodomesticos;
 public class Lavadora extends Electrodomestico {
 	
 	
-	// atributos 
+	/**
+	 * Atributo por defecto
+	 */
 	
-	private final static int CARGA= 5;
-
-
+	private final static int CARGA = 5;
+	
 
 	/**
-	 * Atributos por defecto.
+	 * Atributo
 	 */
 	
 	
-		int carga;
+	int carga;
 	
 		
 	/**
-	 * Contructores vacio
+	 * Contructores por defecto
 	 */
 		
-		
-	// Constructor
-		
-	public Lavadora() {
-		
-	}
-		
-	public Lavadora(int precioBase, String color, String consumoEnergetico, int peso) {
-		super(precioBase, color, consumoEnergetico, peso);
-		this.carga = carga;
+	public Lavadora(int carga) {
+		super();
+		this.carga = CARGA;
 	}
 	
+	
+	// constructor con precio y peso, el resto por defecto
+	public Lavadora(int precioBase, int peso) {
+		super(precioBase, peso);
+		this.carga = CARGA;
+	
+	}
+	
+	// contructor con carga y resto heredados
 	public Lavadora(int precioBase, String color, String consumoEnergetico, int peso) {
-		super(precioBase, color, consumoEnergetico, peso);
-		this.super().precioBase = precioBase;
-		this.super().peso = peso;
+		super(precioBase, color, consumoEnergetico,peso);
+		this.carga = CARGA;
+	
 	}
 	
 
+	public int getCarga() {
+		return carga;
+	}
+
 	
-		
-		
-		
 	
 	
+	
+	
+	
+	// MEtodo Calcula que la carga sea mayor o menor a 30
+	public int precioFinal(int precio){
+		int precioRetorno = 0;
+		if (carga>=30) {
+			precioRetorno = precioRetorno + 50;
+		}else if (carga<30) {
+			precioRetorno = precioRetorno + 0;
+		}
+		return precio;
+		
+	}
+
 }
 
 
