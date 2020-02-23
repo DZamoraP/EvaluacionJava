28f1642e6a8e23a28f10b2396b1094a9e280093b
Daniel Zamora <danielz.informatico@gmail.com>
2020-02-22 21:46:05 -0300

    clase proncipal ejecutable

--- src/Electrodomesticos/principal.java
+++ src/Electrodomesticos/principal.java
@@ -5,6 +5,20 @@ public class principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//Electrodomestico[] arreglo  = new Electrodomestico[9];
+		
+		Electrodomestico arreglo[]= {new Electrodomestico(100,"negro","A",19), new Electrodomestico(100,"rojo","A",19),
+				new Electrodomestico(100,"azul","A",19),new Electrodomestico(100,"gris","A",19),
+				new Electrodomestico(100,"blanco","A",19),new Electrodomestico(100,"gris","A",19),
+				new Electrodomestico(100,"azul","A",19),new Electrodomestico(100,"rojo","A",19),
+				new Electrodomestico(100,"azul","A",19),new Electrodomestico(100,"negro","A",19)};
+		
+		for (int i=0; i<arreglo.length;i++) {
+			System.out.println(arreglo[i]);
+		}
+		
+
+		
 	}
 
 }
